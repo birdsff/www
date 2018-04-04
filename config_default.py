c0cd9bc63e32d8b2150cb4720ee02a626d423664
birdsff <shifufeng@hotmail.com>
2018-04-04 17:09:12 +0800

    0404day11
    error on day 11

---
+++ config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'sff Michael Liao'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'blogs',
+        'password': 'with2018)$)@',
+        'db': 'blogs'
+    },
+    'session': {
+        'secret': 'Blogs'
+    }
+}
